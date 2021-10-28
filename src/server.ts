df52329781639ef39d5338b300135d6e970681c0
Derkian Afonso <{ID}+{username}@users.noreply.github.com>
2021-10-28 00:10:14 -0300

    feat: Configuração para debugger

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,11 @@ import express from "express";
 import { createCourse } from "./routes";
 
 const app = express();
+app.use(express.json());
 app.get("/", createCourse);
-app.listen(3333, () => console.log("Server is running!!"));
+
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+  return response.json({ name });
+});
+app.listen(3333, () => console.log("Server is running!"));
